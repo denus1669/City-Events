0d8a5baf3ac3be13628feb5a4f6ecb6f28f0bf08
densuh <76003514+denus1669@users.noreply.github.com>
2026-03-26 20:10:38 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Документация API городских событий</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>
+    window.onload = () => {
+      SwaggerUIBundle({
+        url: "/OpenAPI/openapi.yaml",   
+        dom_id: '#swagger-ui',
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIBundle.SwaggerUIStandalonePreset
+        ],
+        layout: "BaseLayout",
+      });
+    };
+  </script>
+</body>
+</html>
